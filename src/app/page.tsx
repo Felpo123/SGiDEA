16025d78ff40b1f4ffad0d367953290179685c1b
Felpo123 <felpokun123@gmail.com>
2023-10-14 16:48:35 -0300

    feat: Basic implementation of NextAuth sign out and sign in for prove functionalitys

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,30 @@
 "use client";
 
 import { useSession } from "next-auth/react";
-import Image from "next/image";
+import { signIn, signOut } from "next-auth/react"
 
 export default function Home() {
-  const { data, status } = useSession();
-  console.log(data?.user, status);
+  const { data: session, status } = useSession();  
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Hello, world!
+      {status === "authenticated" && (
+        <div className="flex flex-col items-center justify-center">          
+          <h1 className="text-2xl font-bold">{session.user.fullname}</h1>
+          <p className="text-gray-500">{session.user.email}</p>
+          <button onClick={() => signOut()}>Sign out</button>
+        </div>
+      )}
+      {
+        status === "unauthenticated" && (
+          <div className="flex flex-col items-center justify-center">
+            <h1 className="text-2xl font-bold">You are not logged in</h1>
+            <button onClick={() => signIn()}>Sign in</button>
+          </div>
+        )
+      }
+      
     </main>
   );
 }
