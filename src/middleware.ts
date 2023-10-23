ef677e671259131f2fc771e8d0f8db15c392607a
Felipe Castro <77942887+Felpo123@users.noreply.github.com>
2023-10-24 00:10:31 +0200

    feat: add role based auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,24 @@
-export { default } from "next-auth/middleware";
+import { NextRequestWithAuth, withAuth } from "next-auth/middleware";
+import { appRoutes } from "./routes";
+import { NextResponse } from "next/server";
+import { Role } from "./types/role";
 
-export const config = { matcher: ["/"] };
+export default withAuth(
+  function middleware(request: NextRequestWithAuth) {
+    if (
+      request.nextUrl.pathname === appRoutes.users &&
+      request.nextauth.token?.user.role !== Role.ADMINISTRADOR
+    ) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  }
+);
+
+export const config = {
+  matcher: ["/", "/objects", "/assignments", "/users", "/api/:path*"],
+};
