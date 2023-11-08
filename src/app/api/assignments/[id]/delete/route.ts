e51197905de32484e8ae94e8150373caf593316b
Maximiliano Divin <m.divin01@ufromail.cl>
2023-11-08 11:29:35 -0300

    set objects and assginments flags to false (delete)

---
+++ src/app/api/assignments/[id]/delete/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+
+interface Params {
+    params: { id: string }
+}
+
+export async function PUT(request:Request,{params}:Params){
+    try{
+       const assignment = await prisma.assignments.update({where: {id: parseInt(params.id)}, data: {flag: false}})
+        return NextResponse.json(assignment);
+    }catch(e){
+        if(e instanceof Error){
+            return NextResponse.json(
+                {
+                    message: e.message,
+                },
+                {
+                    status: 500,
+                }
+            );
+        }
+    }
+}
