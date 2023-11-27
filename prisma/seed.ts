0364b9ac49b19620f2edbaaec06de5c24b5748e8
Felpo123 <felpokun123@gmail.com>
2023-11-27 17:52:14 -0300

    fix: delete character

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -139,5 +139,4 @@ async function main() {
 main().catch(e =>{
     console.error(e);
     process.exit(1);
-}).finally(async () =>    await prisma.$disconnect();
-);
+}).finally(async () => {await prisma.$disconnect()});
